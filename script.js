7dd04be37d93b4977b2aeaa85fe3cf335346d9e0
Jeffrey <69681552+Jeffrey9427@users.noreply.github.com>
2024-02-06 15:28:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const url = "https://api.adviceslip.com/advice";
+
+const adviceNo = document.getElementById("advice-no");
+const adviceText = document.getElementById("advice-text");
+const diceButton = document.querySelector(".dice");
+
+const fetchAdvice = async() => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    adviceNo.textContent = `ADVICE #${data.slip.id}`
+    adviceText.textContent = `"${data.slip.advice}"`
+}
+
+fetchAdvice()
+
+diceButton.addEventListener("click", fetchAdvice);
